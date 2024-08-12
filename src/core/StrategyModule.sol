f94016d33ce5d4bd5522d55715c582a85e7da461
asanson <alexandresanson.pro@gmail.com>
2024-08-12 20:35:20 +0800

    feat: StrategyModuleStorage contract + gap for upgradeability

--- src/core/StrategyModule.sol
+++ src/core/StrategyModule.sol
@@ -3,54 +3,15 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin-upgrades/contracts/proxy/utils/Initializable.sol";
 
-import "eigenlayer-contracts/interfaces/IEigenPodManager.sol";
-import "eigenlayer-contracts/interfaces/IEigenPod.sol";
-import "eigenlayer-contracts/interfaces/IDelegationManager.sol";
 import "eigenlayer-contracts/interfaces/ISignatureUtils.sol";
 import "eigenlayer-contracts/libraries/BeaconChainProofs.sol";
 
-import "../interfaces/IByzNft.sol";
-import "../interfaces/IStrategyModuleManager.sol";
-import "../interfaces/IStrategyModule.sol";
-import "../interfaces/IAuction.sol";
+import "./StrategyModuleStorage.sol";
 
 // TODO: Allow Strategy Module ByzNft to be tradeable => conceive a fair exchange mechanism between the seller and the buyer
 
-contract StrategyModule is IStrategyModule, Initializable {
+contract StrategyModule is Initializable, StrategyModuleStorage {
     using BeaconChainProofs for *;
-
-    /* ============== CONSTANTS + IMMUTABLES ============== */
-
-    /// @notice Average time for block finality in the Beacon Chain
-    uint16 internal constant FINALITY_TIME = 16 minutes;
-
-    uint8 internal constant CLUSTER_SIZE = 4;
-
-    /// @notice The single StrategyModuleManager for Byzantine
-    IStrategyModuleManager public immutable stratModManager;
-
-    /// @notice ByzNft contract
-    IByzNft public immutable byzNft;
-
-    /// @notice Address of the Auction contract
-    IAuction public immutable auction;
-
-    /// @notice EigenLayer's EigenPodManager contract
-    /// @dev this is the pod manager transparent proxy
-    IEigenPodManager public immutable eigenPodManager;
-
-    /// @notice EigenLayer's DelegationManager contract
-    IDelegationManager public immutable delegationManager;
-
-    /* ============== STATE VARIABLES ============== */
-
-    /// @notice The ByzNft associated to this StrategyModule.
-    /// @notice The owner of the ByzNft is the StrategyModule owner.
-    /// TODO When non-upgradeable put that variable immutable and set it in the constructor
-    uint256 public stratModNftId;
-
-    // Empty struct, all the fields have their default value
-    ClusterDetails public clusterDetails;
 
     /* ============== MODIFIERS ============== */
 
