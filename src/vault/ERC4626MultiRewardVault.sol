98a75590e11f1a73c1ce3535fd066920a41218d6
Tranquil-Flow <66773372+Tranquil-Flow@users.noreply.github.com>
2024-09-04 00:10:45 +1000

    feat: init ERC4626MultiRewardVault

---
+++ src/vault/ERC4626MultiRewardVault.sol
@@ -0,0 +1,72 @@
+pragma solidity ^0.8.20;
+
+import "@openzeppelin-upgrades/contracts/token/ERC20/extensions/ERC4626Upgradeable.sol";
+import "@openzeppelin-upgrades/contracts/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin-upgrades/contracts/access/OwnableUpgradeable.sol";
+import "@openzeppelin-upgrades/contracts/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
+
+contract ERC4626MultiRewardVault is Initializable, ERC4626Upgradeable, OwnableUpgradeable {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    IERC20Upgradeable[] public rewardTokens;
+
+    function initialize(IERC20Upgradeable _asset) public initializer {
+        string memory assetSymbol = IERC20MetadataUpgradeable(address(_asset)).symbol();
+        string memory vaultName = string(abi.encodePacked(assetSymbol, " Byzantine StrategyVault Token"));
+        string memory vaultSymbol = string(abi.encodePacked("bv", assetSymbol));
+
+        __ERC4626_init(IERC20MetadataUpgradeable(address(_asset)));
+        __ERC20_init(vaultName, vaultSymbol);
+        __Ownable_init();
+    }
+
+    function addRewardToken(IERC20Upgradeable _rewardToken) external onlyOwner {
+        rewardTokens.push(_rewardToken);
+    }
+
+    function _withdraw(
+        address caller,
+        address receiver,
+        address owner,
+        uint256 assets,
+        uint256 shares
+    ) internal override {
+        super._withdraw(caller, receiver, owner, assets, shares);
+        _distributeRewards(receiver, shares);
+    }
+
+    function _distributeRewards(address receiver, uint256 sharesBurned) internal {
+        uint256 totalShares = totalSupply();
+        for (uint i = 0; i < rewardTokens.length; i++) {
+            IERC20Upgradeable rewardToken = rewardTokens[i];
+            uint256 rewardBalance = rewardToken.balanceOf(address(this));
+            uint256 rewardAmount = (rewardBalance * sharesBurned) / totalShares;
+            if (rewardAmount > 0) {
+                rewardToken.safeTransfer(receiver, rewardAmount);
+            }
+        }
+    }
+
+    function totalAssets() public view override returns (uint256) {
+        uint256 totalValue = super.totalAssets();
+        for (uint i = 0; i < rewardTokens.length; i++) {
+            IERC20Upgradeable rewardToken = rewardTokens[i];
+            totalValue += rewardToken.balanceOf(address(this));
+        }
+        return totalValue;
+    }
+
+    function convertToShares(uint256 assets) public view override returns (uint256) {
+        uint256 supply = totalSupply();
+        return (assets == 0 || supply == 0)
+            ? assets
+            : (assets * supply) / totalAssets();
+    }
+
+    function convertToAssets(uint256 shares) public view override returns (uint256) {
+        uint256 supply = totalSupply();
+        return (supply == 0)
+            ? shares 
+            :(shares * totalAssets()) / supply;
+    }
+}
