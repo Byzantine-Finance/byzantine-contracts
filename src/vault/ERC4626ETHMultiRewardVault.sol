bfa2300414805aee1a18cf6ddef2e0b9993894d6
Tranquil-Flow <66773372+Tranquil-Flow@users.noreply.github.com>
2024-09-11 16:50:38 +1000

    docs: Natspec for ERC4626MultiRewardVaults

--- src/vault/ERC4626ETHMultiRewardVault.sol
+++ src/vault/ERC4626ETHMultiRewardVault.sol
@@ -10,9 +10,20 @@ import "@openzeppelin-upgrades/contracts/security/ReentrancyGuardUpgradeable.sol
 contract ERC4626MultiRewardVault is Initializable, ERC4626Upgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
+    /* ============== STATE VARIABLES ============== */
+
+    /// @notice List of reward tokens
     IERC20Upgradeable[] public rewardTokens;
+
+    /// @notice Mapping of reward token to its decimals
     mapping(IERC20Upgradeable => uint8) public rewardTokenDecimals;
 
+    /* ============== CONSTRUCTOR & INITIALIZER ============== */
+
+    /**
+     * @notice Used to initialize the ERC4626MultiRewardVault given it's setup parameters.
+     * @dev ETH is used as the asset for this vault, represented as 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE.
+     */
     function initialize() public initializer {
         __ERC4626_init(IERC20MetadataUpgradeable(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE));
         __ERC20_init("ETH Byzantine StrategyVault Token", "byzETH");
@@ -20,16 +31,32 @@ contract ERC4626MultiRewardVault is Initializable, ERC4626Upgradeable, OwnableUp
         __ReentrancyGuard_init();
     }
 
+    /* =================== FALLBACK =================== */
+    /**
+     * @notice Payable fallback function that receives ether deposited to the StrategyVault contract
+     */
     receive() external payable {
         // TODO: emit an event to notify
     }
 
+    /* ============== EXTERNAL FUNCTIONS ============== */
+
+    /**
+     * @notice Adds a reward token to the vault.
+     * @param _rewardToken The reward token to add.
+     */
     function addRewardToken(IERC20Upgradeable _rewardToken) external onlyOwner {
         rewardTokens.push(_rewardToken);
         uint8 decimals = IERC20MetadataUpgradeable(address(_rewardToken)).decimals();
         rewardTokenDecimals[_rewardToken] = decimals;
     }
 
+    /**
+     * @notice Deposits ETH into the vault.
+     * @param assets The amount of ETH being deposit.
+     * @param receiver The address to receive the deposited assets.
+     * @return The amount of shares minted.
+     */
     function deposit(uint256 assets, address receiver) public virtual override payable nonReentrant returns (uint256) {
         require(msg.value == assets, "Incorrect ETH amount");
         uint256 shares = previewDeposit(assets);
@@ -37,6 +64,13 @@ contract ERC4626MultiRewardVault is Initializable, ERC4626Upgradeable, OwnableUp
         return shares;
     }
 
+    /**
+     * @notice Withdraws ETH from the vault.
+     * @param assets The amount of ETH to withdraw.
+     * @param receiver The address to receive the withdrawn assets.
+     * @param owner The address that is withdrawing the assets.
+     * @return The amount of shares burned.
+     */
     function withdraw(uint256 assets, address receiver, address owner) public virtual override nonReentrant returns (uint256) {
         uint256 shares = previewWithdraw(assets);
         _withdraw(msg.sender, receiver, owner, assets, shares);
@@ -45,6 +79,51 @@ contract ERC4626MultiRewardVault is Initializable, ERC4626Upgradeable, OwnableUp
         return shares;
     }
 
+    /* ================ VIEW FUNCTIONS ================ */
+
+    /**
+     * @notice Returns the total assets in the vault.
+     * @return The total value of assets in the vault.
+     * @dev This function is overridden to integrate with an oracle to determine the total value of all tokens in the vault.
+     * @dev This ensures that when depsoiting or withdrawing, a user receives the correct amount of assets or shares.
+     */
+    function totalAssets() public view override returns (uint256) {
+        uint256 totalValue = super.totalAssets();
+        // TODO: Integrate with oracle to determine total value of all tokens in vault
+        return totalValue;
+    }
+
+    /**
+     * @notice Converts assets to shares.
+     * @param assets The amount of assets to convert.
+     * @return The amount of shares.
+     */ 
+    function convertToShares(uint256 assets) public view override returns (uint256) {
+        uint256 supply = totalSupply();
+        return (assets == 0 || supply == 0)
+            ? assets
+            : (assets * supply) / totalAssets();
+    }
+
+    /**
+     * @notice Converts shares to assets.
+     * @param shares The amount of shares to convert.
+     * @return The amount of assets.
+     */
+    function convertToAssets(uint256 shares) public view override returns (uint256) {
+        uint256 supply = totalSupply();
+        return (supply == 0)
+            ? shares 
+            :(shares * totalAssets()) / supply;
+    }
+
+    /* ============== INTERNAL FUNCTIONS ============== */
+
+    /**
+     * @notice Distributes rewards to the receiver for all rewardTokens.
+     * @param receiver The address to receive the rewards.
+     * @param sharesBurned The amount of shares burned.
+     */
     function _distributeRewards(address receiver, uint256 sharesBurned) internal {
         uint256 totalShares = totalSupply();
         for (uint i = 0; i < rewardTokens.length; i++) {
@@ -57,23 +136,4 @@ contract ERC4626MultiRewardVault is Initializable, ERC4626Upgradeable, OwnableUp
         }
     }
 
-    function totalAssets() public view override returns (uint256) {
-        uint256 totalValue = super.totalAssets();
-        // TODO: Integrate with oracle to determine total value of all tokens in vault
-        return totalValue;
-    }
-
-    function convertToShares(uint256 assets) public view override returns (uint256) {
-        uint256 supply = totalSupply();
-        return (assets == 0 || supply == 0)
-            ? assets
-            : (assets * supply) / totalAssets();
-    }
-
-    function convertToAssets(uint256 shares) public view override returns (uint256) {
-        uint256 supply = totalSupply();
-        return (supply == 0)
-            ? shares 
-            :(shares * totalAssets()) / supply;
-    }
 }
--- src/vault/ERC4626MultiRewardVault.sol
+++ src/vault/ERC4626MultiRewardVault.sol
@@ -10,9 +10,20 @@ import "@openzeppelin-upgrades/contracts/security/ReentrancyGuardUpgradeable.sol
 contract ERC4626MultiRewardVault is Initializable, ERC4626Upgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
+    /* ============== STATE VARIABLES ============== */
+
+    /// @notice List of reward tokens
     IERC20Upgradeable[] public rewardTokens;
+
+    /// @notice Mapping of reward token to its decimals
     mapping(IERC20Upgradeable => uint8) public rewardTokenDecimals;
 
+    /* ============== CONSTRUCTOR & INITIALIZER ============== */
+
+    /**
+     * @notice Used to initialize the ERC4626MultiRewardVault given it's setup parameters.
+     * @dev ETH is used as the asset for this vault, represented as 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE.
+     */
     function initialize(IERC20Upgradeable _asset) public initializer {
         string memory assetSymbol = IERC20MetadataUpgradeable(address(_asset)).symbol();
         string memory vaultName = string(abi.encodePacked(assetSymbol, " Byzantine StrategyVault Token"));
@@ -24,18 +35,76 @@ contract ERC4626MultiRewardVault is Initializable, ERC4626Upgradeable, OwnableUp
         __ReentrancyGuard_init();
     }
 
+    /* ============== EXTERNAL FUNCTIONS ============== */
+
+    /**
+     * @notice Adds a reward token to the vault.
+     * @param _rewardToken The reward token to add.
+     */
     function addRewardToken(IERC20Upgradeable _rewardToken) external onlyOwner {
         rewardTokens.push(_rewardToken);
         uint8 decimals = IERC20MetadataUpgradeable(address(_rewardToken)).decimals();
         rewardTokenDecimals[_rewardToken] = decimals;
     }
 
+    /**
+     * @notice Withdraws assets from the vault.
+     * @param assets The amount of assets to withdraw.
+     * @param receiver The address to receive the withdrawn assets.
+     * @param owner The address that is withdrawing the assets.
+     * @return The amount of shares burned.
+     */
     function withdraw(uint256 assets, address receiver, address owner) public virtual override nonReentrant returns (uint256) {
         uint256 shares = super.withdraw(assets, receiver, owner);
         _distributeRewards(receiver, shares);
         return shares;
     }
 
+    /* ================ VIEW FUNCTIONS ================ */
+
+    /**
+     * @notice Returns the total assets in the vault.
+     * @return The total value of assets in the vault.
+     * @dev This function is overridden to integrate with an oracle to determine the total value of all tokens in the vault.
+     * @dev This ensures that when depsoiting or withdrawing, a user receives the correct amount of assets or shares.
+     */
+    function totalAssets() public view override returns (uint256) {
+        uint256 totalValue = super.totalAssets();
+        // TODO: Integrate with oracle to determine total value of all tokens in vault
+        return totalValue;
+    }
+
+    /**
+     * @notice Converts assets to shares.
+     * @param assets The amount of assets to convert.
+     * @return The amount of shares.
+     */
+    function convertToShares(uint256 assets) public view override returns (uint256) {
+        uint256 supply = totalSupply();
+        return (assets == 0 || supply == 0)
+            ? assets
+            : (assets * supply) / totalAssets();
+    }
+
+    /**
+     * @notice Converts shares to assets.
+     * @param shares The amount of shares to convert.
+     * @return The amount of assets.
+     */
+    function convertToAssets(uint256 shares) public view override returns (uint256) {
+        uint256 supply = totalSupply();
+        return (supply == 0)
+            ? shares 
+            :(shares * totalAssets()) / supply;
+    }
+
+    /* ============== INTERNAL FUNCTIONS ============== */
+
+    /**
+     * @notice Distributes rewards to the receiver for all rewardTokens.
+     * @param receiver The address to receive the rewards.
+     * @param sharesBurned The amount of shares burned.
+     */
     function _distributeRewards(address receiver, uint256 sharesBurned) internal {
         uint256 totalShares = totalSupply();
         for (uint i = 0; i < rewardTokens.length; i++) {
@@ -48,23 +117,4 @@ contract ERC4626MultiRewardVault is Initializable, ERC4626Upgradeable, OwnableUp
         }
     }
 
-    function totalAssets() public view override returns (uint256) {
-        uint256 totalValue = super.totalAssets();
-        // TODO: Integrate with oracle to determine total value of all tokens in vault
-        return totalValue;
-    }
-
-    function convertToShares(uint256 assets) public view override returns (uint256) {
-        uint256 supply = totalSupply();
-        return (assets == 0 || supply == 0)
-            ? assets
-            : (assets * supply) / totalAssets();
-    }
-
-    function convertToAssets(uint256 shares) public view override returns (uint256) {
-        uint256 supply = totalSupply();
-        return (supply == 0)
-            ? shares 
-            :(shares * totalAssets()) / supply;
-    }
 }
