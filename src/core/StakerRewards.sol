57e72e197ef66ceda5e043731dabf10718b12770
lin <lin-h@hotmail.fr>
2024-10-10 20:20:46 +0200

    feat: add variable

--- src/core/StakerRewards.sol
+++ src/core/StakerRewards.sol
@@ -66,6 +66,13 @@ contract StakerRewards is
     /// @notice StratVaultETH address => VaultData
     mapping(address => VaultData) internal _vaults;
 
+    /**
+    * @dev This empty reserved space is put in place to allow future versions to add new
+    * variables without shifting down storage in the inheritance chain.
+    * See https://docs.openzeppelin.com/upgrades-plugins/1.x/writing-upgradeable#modifying-your-contracts
+    */
+    uint256[44] private __gap;
+
     /* ============== CONSTRUCTOR & INITIALIZER ============== */
 
     /// @custom:oz-upgrades-unsafe-allow constructor
