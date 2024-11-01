fd0ee4554c613c02ee845b3e04924b3b698b5987
Tranquil-Flow <tranquil_flow@protonmail.com>
2024-11-01 16:45:31 +1000

    feat: Update & fix ERC4626MultiRewardVault logic
    Imported logic from ERC7535MRV, making adjustments to account for depositing ERC20 instead of ether. totalAssets returns the total value of assets and the asset value of reward tokens, in the same way that ERC7535MRV returns the total value in ether amount.

--- src/vault/ERC4626MultiRewardVault.sol
+++ src/vault/ERC4626MultiRewardVault.sol
@@ -9,52 +9,54 @@ import "@openzeppelin-upgrades/contracts/security/ReentrancyGuardUpgradeable.sol
 import "@openzeppelin-upgrades/contracts/utils/math/MathUpgradeable.sol";
 import "../interfaces/IOracle.sol";
 
+/**
+ * @title ERC4626MultiRewardVault
+ * @author Byzantine-Finance
+ * @notice ERC-4626: Tokenized Vault with support for multiple reward tokens
+ */
 contract ERC4626MultiRewardVault is Initializable, ERC4626Upgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
     using SafeERC20Upgradeable for IERC20Upgradeable;
     using MathUpgradeable for uint256;
 
     /* ============== STATE VARIABLES ============== */
 
-    struct TokenInfo {
-        address priceFeed;
-        uint8 decimals;
-    }
-
-    /// @notice Mapping of asset token address to its information
-    mapping(IERC20Upgradeable => TokenInfo) public assetInfo;
-
-    /// @notice Mapping of reward token address to its information
-    mapping(IERC20Upgradeable => TokenInfo) public rewardInfo;
-
-    /// @notice List of asset tokens
-    IERC20Upgradeable[] public assetTokens;
-
     /// @notice List of reward tokens
-    IERC20Upgradeable[] public rewardTokens;
+    address[] public rewardTokens;
 
     /// @notice Oracle implementation
     IOracle public oracle;
 
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/upgrades-plugins/1.x/writing-upgradeable#modifying-your-contracts
+     */
+    uint256[44] private __gap;
+
     /* ============== CUSTOM ERRORS ============== */
 
     error TokenAlreadyAdded();
-    error InvalidToken();
+    error InvalidAddress();
 
     /* ============== EVENTS ============== */
 
-    event AssetTokenAdded(IERC20Upgradeable indexed token, address priceFeed, uint8 decimals);
-    event RewardTokenAdded(IERC20Upgradeable indexed token, address priceFeed, uint8 decimals);
-    event PriceFeedUpdated(IERC20Upgradeable indexed token, address newPriceFeed);
+    event RewardTokenAdded(address indexed token);
+    event OracleUpdated(address newOracle);
+    event RewardTokenWithdrawn(address indexed receiver, address indexed rewardToken, uint256 amount);
 
     /* ============== CONSTRUCTOR & INITIALIZER ============== */
 
     /**
      * @notice Used to initialize the ERC4626MultiRewardVault given it's setup parameters.
-     * @param _asset The asset to be staked.
      * @param _oracle The oracle implementation address to use for the vault.
+     * @param _asset The asset to be staked.
      */
-    function initialize(IERC20Upgradeable _asset, address _oracle) public initializer {
-        string memory assetSymbol = IERC20MetadataUpgradeable(address(_asset)).symbol();
+    function initialize(address _oracle, address _asset) public initializer {
+        __ERC4626MultiRewardVault_init(_oracle, _asset);
+    }
+
+    function __ERC4626MultiRewardVault_init(address _oracle, address _asset) internal onlyInitializing {
+        string memory assetSymbol = IERC20MetadataUpgradeable(_asset).symbol();
         string memory vaultName = string(abi.encodePacked(assetSymbol, " Byzantine StrategyVault Token"));
         string memory vaultSymbol = string(abi.encodePacked("bvz", assetSymbol));
 
@@ -62,32 +64,92 @@ contract ERC4626MultiRewardVault is Initializable, ERC4626Upgradeable, OwnableUp
         __ERC20_init(vaultName, vaultSymbol);
         __Ownable_init();
         __ReentrancyGuard_init();
+        __ERC4626MultiRewardVault_init_unchained(_oracle);
+    }
+
+    function __ERC4626MultiRewardVault_init_unchained(address _oracle) internal onlyInitializing {
+        oracle = IOracle(_oracle);
+    }
+
+    /* =================== FALLBACK =================== */
+
+    /**
+     * @notice Payable fallback function that receives ether deposited to the StrategyVault contract
+     * @dev Strategy Vault is the address where to send the principal ethers post exit.
+     */
+    receive() external virtual payable {
+        // TODO: emit an event to notify
     }
 
     /* ============== EXTERNAL FUNCTIONS ============== */
 
-    // /**
-    //  * @notice Adds a reward token to the vault.
-    //  * @param _rewardToken The reward token to add.
-    //  */
-    // function addRewardToken(IERC20Upgradeable _rewardToken) external onlyOwner {
-    //     rewardTokens.push(_rewardToken);
-    //     uint8 decimals = IERC20MetadataUpgradeable(address(_rewardToken)).decimals();
-    //     rewardTokenDecimals[_rewardToken] = decimals;
-    // }
+    /**
+     * @notice Deposits assets into the vault in return for vault shares.
+     * @param assets The amount of assets being deposited.
+     * @param receiver The address to receive the Byzantine vault shares.
+     * @return The amount of shares minted.
+     */
+    function deposit(uint256 assets, address receiver) public virtual override returns (uint256) {
+        return super.deposit(assets, receiver);
+    }
 
     /**
-     * @notice Withdraws assets from the vault.
-     * @param assets The amount of assets to withdraw.
-     * @param receiver The address to receive the withdrawn assets.
-     * @param owner The address that is withdrawing the assets.
+     * @notice Deposits assets into the vault. Amount is determined by number of shares minting.
+     * @param shares The amount of shares to mint.
+     * @param receiver The address to receive the Byzantine vault shares.
+     * @return The amount of assets deposited.
+     */
+    function mint(uint256 shares, address receiver) public virtual override returns (uint256) {
+        return super.mint(shares, receiver);
+    }
+
+    /**
+     * @notice Withdraws assets and reward tokens from the vault. Amount is determined by ETH withdrawing
+     * @dev User proportionally receives assets and reward tokens that are combined worth the amount of `assets` specified.
+     * @param assets The value to withdraw from the vault, in ETH amount.
+     * @param receiver The address to receive the assets.
+     * @param owner The address that is withdrawing assets.
      * @return The amount of shares burned.
      */
-    function withdraw(uint256 assets, address receiver, address owner) public virtual override nonReentrant returns (uint256) {
-        uint256 shares = super.withdraw(assets, receiver, owner);
-        _withdraw(msg.sender, receiver, owner, assets, shares);
-        _distributeRewards(receiver, shares);
-        return shares;
+    function withdraw(uint256 assets, address receiver, address owner) public override nonReentrant returns (uint256) {   
+        // Calculate the amount of shares that will be burned
+        uint256 sharesToBurn = previewWithdraw(assets);
+
+        // Get the total asset value of assets + reward tokens owned by the user
+        uint256 userTotalAssetValue = getUserTotalValue(owner);
+
+        // Calculate the proportion of total value being withdrawn
+        uint256 withdrawProportion = (assets * 1e18) / userTotalAssetValue;
+
+        // Get user's owned assets and rewards
+        (address[] memory tokenAddresses, uint256[] memory tokenAmounts) = getUsersOwnedAssetsAndRewards(owner);
+
+        // Calculate the amount of assets that will be withdrawn, based on the withdrawn proportion
+        uint256 assetsToWithdraw = (tokenAmounts[0] * withdrawProportion) / 1e18;
+        
+        // Withdraw assets
+        uint256 sharesBurnedForAssets = super.withdraw(assetsToWithdraw, receiver, owner);
+        
+        // Burn shares representing reward tokens
+            // withdraw() must ensure that it burns amount of `shares` specified by the user.
+            // If there are reward tokens, user will not have burned all shares in the super.withdraw() call.
+            // If there are no teward tokens, the user will have burned all shares.
+        uint256 sharesBurningForRewardTokens = sharesToBurn - sharesBurnedForAssets;
+        _burn(owner, sharesBurningForRewardTokens);
+
+        // Withdraw proportional amount of each reward token
+        for (uint i = 1; i < tokenAddresses.length; i++) {
+            address token = tokenAddresses[i];
+            uint256 tokenAmount = tokenAmounts[i];
+            uint256 tokenToWithdraw = (tokenAmount * withdrawProportion) / 1e18;
+            if (tokenToWithdraw > 0) {
+                IERC20Upgradeable(token).safeTransfer(receiver, tokenToWithdraw);
+                emit RewardTokenWithdrawn(receiver, token, tokenToWithdraw);
+            }
+        }
+
+        uint256 totalSharesBurned = sharesBurnedForAssets + sharesBurningForRewardTokens;
+        return totalSharesBurned;
     }
 
     /**
@@ -95,70 +157,191 @@ contract ERC4626MultiRewardVault is Initializable, ERC4626Upgradeable, OwnableUp
      * @param shares The amount of shares to burn to exchange for assets.
      * @param receiver The address to receive the assets.
      * @param owner The address that is withdrawing assets.
-     * return The amount of assets withdrawn.
+     * return The amount of assets withdrawn (includes asset + asset value of all reward tokens).
      */
-    function redeem(uint256 shares, address receiver, address owner) public virtual override nonReentrant returns (uint256) {
-        require(shares <= maxRedeem(owner), "ERC4626: redeem more than max");
-        uint256 assets = previewRedeem(shares);
-        _withdraw(_msgSender(), receiver, owner, assets, shares);
-        return assets;
+    function redeem(uint256 shares, address receiver, address owner) public override nonReentrant returns (uint256) {
+        // Calculate the proportion of shares the user owns
+        uint256 userShareProportion = (balanceOf(owner) * 1e18) / totalSupply();
+        
+        // Calculate the proportion of shares the user is redeeming
+        uint256 userWithdrawProportion = (shares * 1e18) / balanceOf(owner);
+
+        // Get user's owned assets and rewards 
+        (address[] memory tokenAddresses, uint256[] memory tokenAmounts) = getUsersOwnedAssetsAndRewards(owner);
+
+        // Calculate amount of assets user is withdrawing
+        uint256 assetsToWithdraw = (tokenAmounts[0] * userWithdrawProportion) / 1e18;
+
+        // Calculate amount of shares to burn to withdraw assets
+        uint256 sharesToBurn = previewWithdraw(assetsToWithdraw);
+
+        // Withdraw assets
+        uint256 assetsWithdrawn = super.redeem(sharesToBurn, receiver, owner);
+        
+        // Burn shares representing reward tokens
+        // redeem() must ensure that it burns amount of `shares` specified by the user.
+        // If there are reward tokens, user will not have burned all shares in the super.redeem() call.
+        // If there are no reward tokens, the user will have burned all shares.
+        uint256 sharesBurningForRewardTokens = shares - sharesToBurn;
+        _burn(owner, sharesBurningForRewardTokens);
+
+        // Withdraw proportional amount of each reward token
+        uint256 totalRewardTokenValueWithdrawn;
+        for (uint i = 0; i < rewardTokens.length; i++) {
+            address token = rewardTokens[i];
+
+            // Get the total amount of the reward token owned by the user
+            uint256 totalTokenOwnedByUser = tokenAmounts[i + 1];
+
+            // Calculate the amount of the reward token to withdraw
+            uint256 tokensToWithdraw = (totalTokenOwnedByUser * userWithdrawProportion) / 1e18;
+
+            if (tokensToWithdraw > 0) {
+                IERC20Upgradeable(token).safeTransfer(receiver, tokensToWithdraw);
+                emit RewardTokenWithdrawn(receiver, token, tokensToWithdraw);
+
+                // Add the ETH value of the withdrawn reward token to the total
+                uint256 tokenPrice = oracle.getPrice(token);
+                uint256 ethPrice = oracle.getPrice(address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE));
+                uint256 rewardTokenValueInEth = (tokensToWithdraw * tokenPrice) / ethPrice;
+
+                totalRewardTokenValueWithdrawn += rewardTokenValueInEth;
+            }
+        }
+
+        uint256 totalValueWithdrawn = assetsWithdrawn + totalRewardTokenValueWithdrawn;
+        return totalValueWithdrawn;
+    }
+
+        /**
+     * @notice Adds a reward token to the vault.
+     * @param _token The reward token to add.
+     */
+    function addRewardToken(address _token) external onlyOwner {
+        // Check if the token is already in the rewardTokens array
+        for (uint i = 0; i < rewardTokens.length; i++) {
+            if (rewardTokens[i] == _token) revert TokenAlreadyAdded();
+        }
+        rewardTokens.push(_token);
+        emit RewardTokenAdded(_token);
+    }
+
+    /**
+     * @notice Updates the oracle implementation address for the vault.
+     * @param _oracle The new oracle implementation address.
+     */
+    function updateOracle(address _oracle) external onlyOwner {
+        if (_oracle == address(0)) revert InvalidAddress();
+        oracle = IOracle(_oracle);
+        emit OracleUpdated(_oracle);
     }
 
     /* ================ VIEW FUNCTIONS ================ */
 
     /**
-     * @notice Returns the total assets in the vault.
-     * @return The total value of assets in the vault.
+     * @notice Returns the total value in the vault.
+     * @return The total value of assets and reward tokens in the vault, in asset token amount.
      * @dev This function is overridden to integrate with an oracle to determine the total value of all tokens in the vault.
-     * @dev This ensures that when depsoiting or withdrawing, a user receives the correct amount of assets or shares.
+     * @dev This ensures that when depositing or withdrawing, a user receives a proportional amount of assets or shares.
+     * @dev Assumes that the oracle returns the price in 18 decimals.
      */
-    function totalAssets() public view override returns (uint256) {
-        uint256 totalValue = super.totalAssets();
-        // TODO: Integrate with oracle to determine total value of all tokens in vault
-        return totalValue;
+    function totalAssets() public view override returns (uint256) {        
+        // Calculate USD value of reward tokens
+        uint256 rewardTokenUSDValue;
+        for (uint i = 0; i < rewardTokens.length; i++) {
+            address token = rewardTokens[i];
+            uint256 balance = IERC20Upgradeable(token).balanceOf(address(this));
+            uint256 price = oracle.getPrice(token);
+            rewardTokenUSDValue += (balance * price);
+        }
+
+        // Convert total value of reward tokens from USD to asset token
+        uint256 assetPrice = oracle.getPrice(address(super.asset()));
+        uint256 rewardTokenAssetAmount = rewardTokenUSDValue / assetPrice;
+
+        // Add the asset value of the reward tokens to the asset balance to get total asset amount
+        uint256 assetBalance = IERC20Upgradeable(super.asset()).balanceOf(address(this));
+        uint256 totalAssetAmount = assetBalance + rewardTokenAssetAmount;
+        return totalAssetAmount;
     }
 
     /**
-     * @notice Converts assets to shares.
-     * @param assets The amount of assets to convert.
-     * @return The amount of shares.
+     * @notice Returns the total asset value of assets and reward tokens in the vault for a user.
+     * @param user The address of the user.
+     * @return The total value of assets in the vault for the user, in asset token amount.
      */
-    function convertToShares(uint256 assets) public view override returns (uint256) {
-        uint256 supply = totalSupply();
-        return (assets == 0 || supply == 0)
-            ? assets
-            : (assets * supply) / totalAssets();
+    function getUserTotalValue(address user) public view returns (uint256) {
+        uint256 userShares = balanceOf(user);
+        uint256 userSharesProportion = (userShares * 1e18) / totalSupply();
+        uint256 userTotalETHValue = (userSharesProportion * totalAssets()) / 1e18;
+        return userTotalETHValue;
     }
 
     /**
-     * @notice Converts shares to assets.
-     * @param shares The amount of shares to convert.
-     * @return The amount of assets.
+     * @notice Returns the assets and reward tokens owned by a user.
+     * @dev The asset amount is the first element of the returned array.
+     * @param user The address of the user.
+     * @return The assets/reward tokens owned by the user and their respective amounts.
      */
-    function convertToAssets(uint256 shares) public view override returns (uint256) {
-        uint256 supply = totalSupply();
-        return (supply == 0)
-            ? shares 
-            :(shares * totalAssets()) / supply;
+    function getUsersOwnedAssetsAndRewards(address user) public view returns (address[] memory, uint256[] memory) {
+        // Get the proportion of shares (and thus assets) the user owns
+        uint256 userShares = balanceOf(user);
+        uint256 userSharesProportion = (userShares * 1e18) / totalSupply();
+
+        // Get the amount of assets owned by the user
+        address asset = super.asset();
+        uint256 userAssetAmount = (userSharesProportion * IERC20Upgradeable(asset).balanceOf(address(this))) / 1e18;
+
+        // Setup arrays for tokens owned by the user
+        address[] memory tokenAddresses = new address[](rewardTokens.length + 1);
+        uint256[] memory tokenAmounts = new uint256[](rewardTokens.length + 1);
+        
+        // Add asset to the arrays
+        tokenAddresses[0] = asset;
+        tokenAmounts[0] = userAssetAmount;
+
+        // Add reward tokens to the arrays
+        for (uint i = 0; i < rewardTokens.length; i++) {
+            address token = rewardTokens[i];
+
+            uint256 vaultBalance = IERC20Upgradeable(token).balanceOf(address(this));
+            uint256 userTokenAmount = (vaultBalance * userSharesProportion) / 1e18;
+
+            tokenAddresses[i + 1] = token;
+            tokenAmounts[i + 1] = userTokenAmount;
+        }
+
+        return (tokenAddresses, tokenAmounts);
     }
 
     /* ============== INTERNAL FUNCTIONS ============== */
 
     /**
-     * @notice Distributes rewards to the receiver for all rewardTokens.
+     * @dev Distributes rewards to the receiver for all rewardTokens.
      * @param receiver The address to receive the rewards.
      * @param sharesBurned The amount of shares burned.
+     * @param totalSharesPreWithdraw The total number of shares before the withdrawal sequence was initiated.
      */
-    function _distributeRewards(address receiver, uint256 sharesBurned) internal {
-        uint256 totalShares = totalSupply();
+    function _distributeRewards(address receiver, uint256 sharesBurned, uint256 totalSharesPreWithdraw) internal {
+        uint256 totalShares = totalSharesPreWithdraw;
+
         for (uint i = 0; i < rewardTokens.length; i++) {
-            IERC20Upgradeable rewardToken = rewardTokens[i];
-            uint256 rewardBalance = rewardToken.balanceOf(address(this));
+            address rewardToken = rewardTokens[i];
+            uint256 rewardBalance = IERC20Upgradeable(rewardToken).balanceOf(address(this));
             uint256 rewardAmount = (rewardBalance * sharesBurned) / totalShares;
             if (rewardAmount > 0) {
-                rewardToken.safeTransfer(receiver, rewardAmount);
+                IERC20Upgradeable(rewardToken).safeTransfer(receiver, rewardAmount);
+                emit RewardTokenWithdrawn(receiver, rewardToken, rewardAmount);
             }
         }
+    }
+
+    /**
+     * @dev Returns the ETH balance of the vault.
+     * @return The ETH balance of the vault.
+     */
+    function _getETHBalance() internal view virtual returns (uint256) {
+        return address(this).balance;
     }
 
 }
