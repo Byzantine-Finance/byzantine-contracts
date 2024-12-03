33680b585b4519099d29e83688d37b9ac9f3c915
Tranquil Flow <tranquil_flow@protonmail.com>
2024-12-03 23:59:31 +1000

    refactor: move vault parameters to state variables + fix errors

--- script/deploy/holesky/Deploy_Symbiotic_Vault.s.sol
+++ script/deploy/holesky/Deploy_Symbiotic_Vault.s.sol
@@ -13,16 +13,18 @@ import {INetworkRestakeDelegator} from "@symbioticfi/core/src/interfaces/delegat
 import {IBaseSlasher} from "@symbioticfi/core/src/interfaces/slasher/IBaseSlasher.sol";
 import {ISlasher} from "@symbioticfi/core/src/interfaces/slasher/ISlasher.sol";
 
-// forge script script/deploy/holesky/Deploy_Symbiotic_Vault.s.sol:ByzantineSymbioticVaultDeployer --rpc-url https://ethereum-holesky-rpc.publicnode.com --chain holesky --broadcast -vvv
-contract ByzantineSymbioticVaultDeployer is Script {
-
+/**
+ * @notice Script used to deploy a Byzantine Symbiotic Vault to Holesky
+ * forge script script/deploy/holesky/Deploy_Symbiotic_Vault.s.sol:DeploySymbioticVault --rpc-url https://ethereum-holesky-rpc.publicnode.com --chain holesky --broadcast -vvv
+ */
+contract DeploySymbioticVault is Script {
     // BurnerRouter parameters
     address public constant BURNER_ROUTER_FACTORY = 0x32e2AfbdAffB1e675898ABA75868d92eE1E68f3b;
     address public constant OWNER = 0xe8616DEcea16b5216e805B0b8caf7784de7570E7;
     address public constant COLLATERAL = 0x8d09a4502Cc8Cf1547aD300E066060D043f6982D;
     uint48 public constant DELAY = 1814400;
     address public constant GLOBAL_RECEIVER = 0x25133c2c49A343F8312bb6e896C1ea0Ad8CD0EBd;
-    
+
     // Vault parameters
     address public constant VAULT_CONFIGURATOR = 0xD2191FE92987171691d552C219b8caEf186eb9cA;
     uint48 public constant EPOCH_DURATION = 7 days;
@@ -32,17 +34,22 @@ contract ByzantineSymbioticVaultDeployer is Script {
     bool public constant WITH_SLASHER = true;
     uint64 public constant SLASHER_INDEX = 0;
     uint48 public constant VETO_DURATION = 1 days;
-    
+
+    // Vault initialization parameters
+    uint64 public constant VERSION = 1;
+    bool public constant DEPOSIT_WHITELIST = false;
+    bool public constant IS_DEPOSIT_LIMIT = true;
+    address public constant DEFAULT_ADMIN_ROLE = OWNER;
+    address public constant DEPOSIT_WHITELIST_SET_ROLE = OWNER;
+    address public constant DEPOSITOR_WHITELIST_ROLE = OWNER;
+    address public constant IS_DEPOSIT_LIMIT_SET_ROLE = OWNER;
+    address public constant DEPOSIT_LIMIT_SET_ROLE = OWNER;
+
     // DefaultStakerRewards parameters
     address public constant DEFAULT_STAKER_REWARDS_FACTORY = 0x698C36DE44D73AEfa3F0Ce3c0255A8667bdE7cFD;
     uint256 public constant ADMIN_FEE = 1000; // 10% (assuming base 10000)
     
-    // Network Limit parameters
-    bytes32 public constant NETWORK_ID = "holesky";
-    uint256 public constant NETWORK_LIMIT = 1000 ether;
-    
     function run() public {
-        address sender = vm.envAddress("SENDER");
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(privateKey);
         
@@ -58,26 +65,13 @@ contract ByzantineSymbioticVaultDeployer is Script {
             })
         );
         console2.log("Burner Router deployed at:", burnerRouter);
-        
+
         // 2. Deploy Vault
-        address[] memory whitelistedDepositors = new address[](0);
         (address vault, address delegator, address slasher) = IVaultConfigurator(VAULT_CONFIGURATOR).create(
             IVaultConfigurator.InitParams({
-                version: 1,
+                version: VERSION,
                 owner: OWNER,
-                vaultParams: abi.encode(
-                    COLLATERAL,         // collateral
-                    burnerRouter,       // burner
-                    EPOCH_DURATION,     // epochDuration
-                    false,              // depositWhitelist
-                    true,               // isDepositLimit
-                    DEPOSIT_LIMIT,      // depositLimit
-                    OWNER,              // defaultAdminRoleHolder
-                    OWNER,              // depositWhitelistSetRoleHolder
-                    OWNER,              // depositorWhitelistRoleHolder
-                    OWNER,              // isDepositLimitSetRoleHolder
-                    OWNER               // depositLimitSetRoleHolder
-                ),
+                vaultParams: _getVaultParams(burnerRouter),
                 delegatorIndex: DELEGATOR_INDEX,
                 delegatorParams: _getDelegatorParams(),
                 withSlasher: WITH_SLASHER,
@@ -88,7 +82,7 @@ contract ByzantineSymbioticVaultDeployer is Script {
         console2.log("Vault deployed at:", vault);
         console2.log("Delegator deployed at:", delegator);
         console2.log("Slasher deployed at:", slasher);
-        
+
         // 3. Deploy DefaultStakerRewards
         address defaultStakerRewards = IDefaultStakerRewardsFactory(DEFAULT_STAKER_REWARDS_FACTORY).create(
             IDefaultStakerRewards.InitParams({
@@ -103,7 +97,24 @@ contract ByzantineSymbioticVaultDeployer is Script {
         
         vm.stopBroadcast();
     }
-    function _getDelegatorParams() internal view returns (bytes memory) {
+
+    function _getVaultParams(address burnerRouter) internal pure returns (bytes memory) {
+        return abi.encode(
+            COLLATERAL,                  // collateral
+            burnerRouter,                // burner
+            EPOCH_DURATION,              // epochDuration
+            DEPOSIT_WHITELIST,           // depositWhitelist
+            IS_DEPOSIT_LIMIT,            // isDepositLimit
+            DEPOSIT_LIMIT,               // depositLimit
+            DEFAULT_ADMIN_ROLE,          // defaultAdminRoleHolder
+            DEPOSIT_WHITELIST_SET_ROLE,  // depositWhitelistSetRoleHolder
+            DEPOSITOR_WHITELIST_ROLE,    // depositorWhitelistRoleHolder
+            IS_DEPOSIT_LIMIT_SET_ROLE,   // isDepositLimitSetRoleHolder
+            DEPOSIT_LIMIT_SET_ROLE       // depositLimitSetRoleHolder
+        );
+    }
+    
+    function _getDelegatorParams() internal pure returns (bytes memory) {
         address[] memory networkLimitSetRoleHolders = new address[](1);
         networkLimitSetRoleHolders[0] = OWNER;
         address[] memory operatorNetworkSharesSetRoleHolders = new address[](1);
@@ -120,7 +131,8 @@ contract ByzantineSymbioticVaultDeployer is Script {
             })
         );
     }
-    function _getSlasherParams() internal view returns (bytes memory) {
+
+    function _getSlasherParams() internal pure returns (bytes memory) {
         return abi.encode(
             ISlasher.InitParams({
                 baseParams: IBaseSlasher.BaseParams({
