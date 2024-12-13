0ff10cddd6fc83b4d701c835c16982dabdf96ccf
Tranquil-Flow <tranquil_flow@protonmail.com>
2024-12-13 16:28:25 +1000

    fix: Resolve merge conflict issues introduced from main branch
    chore: Downgrade openzeppelin-contracts-upgradeable to 4.9.0, so to allow EigenLayer contracts to be able to use the older version | chore: Install openzeppelin-contracts-upgradeable-v5.1.0 for use by the MRV so they can use the most up to date version of their ERC4626 implementation | chore: Disabled ChainlinkOracleImplementation as this was giving compiler issues due to Chainlink not having a deployed ETH/USD oracle on Holesky, and Chainlink will not be utilized for now | fix: Add required batchSize parameter in ByzNft's _beforeTokenTransfer function | fix: Initialize a mock oracle contract and pass it through when deploying a vault in StakerRewards test

---
+++ script/upgrade/holesky/StrategyVaultETHUpgrade.s.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../../utils/ExistingDeploymentParser.sol";
+
+/**
+ * @notice Script used for upgrading all StrategyVault deployed on Holesky
+ * forge script script/upgrade/holesky/StrategyVaultETHUpgrade.s.sol --rpc-url http://127.0.0.1:8545 --private-key $PRIVATE_KEY --broadcast --etherscan-api-key $ETHERSCAN_API_KEY --verify -vvvv
+ * forge script script/upgrade/holesky/StrategyVaultETHUpgrade.s.sol --rpc-url $HOLESKY_RPC_URL --private-key $PRIVATE_KEY --broadcast --etherscan-api-key $ETHERSCAN_API_KEY --verify -vvvv
+ * 
+ */
+contract StrategyVaultsETHUpgrade is ExistingDeploymentParser {
+    function run() external virtual {
+        _parseInitialDeploymentParams("script/configs/holesky/Deploy_from_scratch.holesky.config.json");
+        _parseDeployedContractAddresses("script/output/holesky/Deploy_from_scratch.holesky.config.json");
+
+        // START RECORDING TRANSACTIONS FOR DEPLOYMENT
+        vm.startBroadcast();
+
+        emit log_named_address("Upgrader Address", msg.sender);
+
+        _upgradeStrategyVaults();
+
+        // STOP RECORDING TRANSACTIONS FOR DEPLOYMENT
+        vm.stopBroadcast();
+
+        // Sanity Checks
+        _verifyContractPointers();
+        _verifyImplementations();
+        _verifyContractsInitialized();
+        _verifyInitializationParams();
+
+        logAndUpdateContractAddresses("script/output/holesky/Deploy_from_scratch.holesky.config.json");
+    }
+
+    /**
+     * @notice Upgrade Auction by deploying a new implementation contract and pointing the proxy to it
+     */
+    function _upgradeStrategyVaults() internal {
+        // Deploy new implementation contract
+        strategyVaultETHImplementation = new StrategyVaultETH(
+            strategyVaultManager,
+            auction,
+            byzNft,
+            eigenPodManager,
+            delegation,
+            stakerRewards,
+            beaconChainAdmin
+        );
+        // Upgrade UpgradeableBeacon
+        strategyVaultETHBeacon.upgradeTo(address(strategyVaultETHImplementation));
+    }
+}
